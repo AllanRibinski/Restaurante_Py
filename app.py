811e0c55e584875bcecb2d85ffbb304bc57d5e8e
AllanRibinski <114584171+AllanRibinski@users.noreply.github.com>
2024-05-03 08:07:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ def listarRestaurantes():
 
 def cadastrar_novo_restaurante():
     nome_do_restaurante = input("Digite o nome do novo restaurante: ")
-    categoria = input(f"digite a categoria do restaurante"(nome_do_restaurante))
+    categoria = input(f"digite a categoria do restaurante (nome_do_restaurante):")
+    dados_do_restaurante = {"nome":nome_do_restaurante, "categoria":categoria, "ativo":false
     restaurantes.append(nome_do_restaurante)
     print(f"Você cadastrou o restaurante: {nome_do_restaurante}")
     voltar_menu_principal()
